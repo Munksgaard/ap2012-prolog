756ed805eda1e9371d6099c8e6dad124cdb95e9c
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-10-02 14:25:13 +0200

    Changed the code such that we always make a check that we are dealing with a natural number OR our set is valid

--- code/sets.pl
+++ code/sets.pl
@@ -1,27 +1,25 @@
 :- module(sets, [less/2, checkset/1, ismember/3, union/3, intersection/3]).
 
+naturalnum(z).
+naturalnum(s(X)) :- naturalnum(X).
+
 % Exercise 1:
 
 less(z, s(_)).
 less(s(X), s(Y)) :- less(X, Y).
 
-
 % Exercise 2:
-
-naturalnum(z).
-naturalnum(s(X)) :- naturalnum(X).
 
 checkset([]).
 checkset([X]) :- naturalnum(X).
 checkset([X, Y | List]) :-
-    naturalnum(X),
     less(X,Y),
     checkset(List).
 
-
 % Exercise 3
+
 ismember(_, [], no).
-ismember(X, [X | _], yes).
+ismember(X, [X | _], yes) :- naturalnum(X).
 ismember(X, [Y | _], no) :-
     less(X,Y).
 ismember(X, [Y | List], Z) :-
@@ -30,8 +28,8 @@ ismember(X, [Y | List], Z) :-
 
 % Exercise 4
 
-union(X, [], X).
-union([], Y, Y).
+union(XS, [], XS) :- checkset(XS).
+union([], YS, YS) :- checkset(YS).
 union([X | XS], [Y | YS], [Y | Z]) :-
     less(Y, X),
     union([X | XS], YS, Z).
@@ -39,14 +37,14 @@ union([X | XS], [Y | YS], [X | Z]) :-
     less(X, Y),
     union([Y | YS], XS, Z).
 union([X | XS], [X | YS], [X | Z]) :-
+    naturalnum(X),
     union(XS, YS, Z).
 
-
 % Exercise 5
-
-intersection(_, [], []).
-intersection([], _, []).
+intersection(XS, [], []) :- checkset(XS).
+intersection([], YS, []) :- checkset(YS).
 intersection([X | XS], [X | YS], [X | Z]) :-
+    naturalnum(X),
     intersection(XS, YS, Z).
 intersection([X | XS], [Y | YS], Z) :-
     less(X, Y),
