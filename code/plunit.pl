1d1b3c5318ca590f16a3238f7035c931c022b8d0
Philip Munksgaard <pmunksgaard@gmail.com>
2012-10-02 13:05:11 +0200

    properly named tests

--- code/plunit.pl
+++ code/plunit.pl
@@ -1,11 +1,13 @@
 :- begin_tests(set_tests).
 :- consult(sets).
 
-test(less, [true]) :- less(z, s(z)).
-test(less, [true]) :- less(s(s(z)), s(s(s(s(z))))).
-test(less, [fail]) :- less(z,z).
-test(less, [fail]) :- less(s(z), z).
+test(less1, [true]) :- less(z, s(z)).
+test(less2, [true]) :- less(s(s(z)), s(s(s(s(z))))).
+test(less3, [fail]) :- less(z,z).
+test(less4, [fail]) :- less(s(z), z).
 
-test(checkset, [true]) :- checkset([z, s(s(z))]).
+test(checkset1, [true]) :- checkset([z, s(s(z))]).
+test(checkset2, [fail]) :- checkset([z, z]).
+test(checkset3, [fail]) :- checkset(z).
 
 :- end_tests(set_tests).
